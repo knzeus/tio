52c75dc4f75254b68b22104c947a4216db262da7
zhoujie <knzeus@qq.com>
2015-04-09 10:40:31 +0800

    add dir for test

---
+++ inc/tio_interface.h
@@ -0,0 +1,42 @@
+/***************************************************************************
+ * 
+ * Copyright (c) 2015 Baidu.com, Inc. All Rights Reserved
+ * $Id$ 
+ * 
+ **************************************************************************/
+ 
+ 
+ 
+/**
+ * @file inc/tio_interface.h
+ * @author knzeus(zhou_jie@baidu.com)
+ * @date 2015/04/09 10:39:07
+ * @version $Revision$ 
+ * @brief 
+ *  
+ **/
+
+
+#ifndef ANTI_THEMIS_INC_TIO_INTERFACE_H
+#define ANTI_THEMIS_INC_TIO_INTERFACE_H
+
+
+void foo() {}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif  // ANTI_THEMIS_INC_TIO_INTERFACE_H
+
+/* vim: set ts=4 sw=4 sts=4 tw=100 */
